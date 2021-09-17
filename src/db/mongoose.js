74542039243f4177d18f10c65e240b70fcfc75cf
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-09-17 22:15:54 +0530

    Task creation route added.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 //Connect to Database
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true
-})
-
-//Task Model
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose.js') //Connect to the database
 const User = require('./models/user.js')
+const Task = require('./models/task.js')
+
 const app = express()
 
 //This convert all incoming JSON to Javascript Object
@@ -8,13 +10,26 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
-//Add a user
+//Add a new user
 app.post('/users', (req, res) => {
   const user = new User(req.body)
 
   user.save()
     .then(() => {
-      res.send(user)
+      res.status(201).send(user)
+    })
+    .catch((e) => {
+      res.status(400).send(e)
+    })
+})
+
+//Add a new Task
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+
+  task.save()
+    .then(() => {
+      res.status(201).send(task)
     })
     .catch((e) => {
       res.status(400).send(e)
