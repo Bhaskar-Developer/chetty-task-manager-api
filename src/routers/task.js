f9530294460604d5897b958876215c5212237ce2
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-10-24 17:06:10 +0530

    Temporary changes pushed to GET all tasks route.

--- src/routers/task.js
+++ src/routers/task.js
@@ -24,41 +24,41 @@ router.post('/tasks', auth, async (req, res) => {
 // GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
   try {
-    let match = {}
-    let sort = {}
+    //let match = {}
+    //let sort = {}
     
     //check if the request query has a paramater as completed
-    if(req.query.completed) {
-      match[completed] = req.query.completed === 'true'
-      //This will return boolean true if req.query.completed matches the string true
-      //This will return boolean false if req.query.completed does not match the string true
-    }
+    // if(req.query.completed) {
+    //   match[completed] = req.query.completed === 'true'
+    //   //This will return boolean true if req.query.completed matches the string true
+    //   //This will return boolean false if req.query.completed does not match the string true
+    // }
 
     //Sort the Data based on the timestamp in ascending or descending order
     //check if request query has a sortBy parameter
-    if(req.query.sortBy) {
+    //if(req.query.sortBy) {
       //break down the sortBy paramater string value into two indivdual words using split() operation.  
       //These individual words will be stored as elements in the parts array
       
-      const parts = req.query.sortBy.split(':') 
+      //const parts = req.query.sortBy.split(':') 
       //The string is split into two when ':' is encountered
       
       //Using ternary operator to check if the value parts[1] is desc or asc
       //If desc then sort[parts[0]] will be -1
       //If asc then sort[parts[0]] will be 1 
-      sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
-    }
+      //sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    //}
     
     //Populate tasks associated with the user based on completed query
-    await req.user.populate({
-      path: 'tasks',
-      match,
-      options: {
-        limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip),
-        sort
-      }
-    })
+    await req.user.populate('tasks')
+      //path: 'tasks'
+      // match,
+      // options: {
+      //   limit: parseInt(req.query.limit),
+      //   skip: parseInt(req.query.skip),
+      //   sort
+      // }
+    //})
     res.send(req.user.tasks)
 
   } catch(e) {
